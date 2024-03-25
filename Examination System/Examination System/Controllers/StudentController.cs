d0031cfd591e9eb42bff1c612460660e7a8fb1f2
Ahmad Esam <90632545+Ahmad-Esam12398@users.noreply.github.com>
2024-03-25 18:03:21 +0200

    resolving conflicts to get latest changes

--- Examination System/Examination System/Controllers/StudentController.cs
+++ Examination System/Examination System/Controllers/StudentController.cs
@@ -61,51 +61,6 @@ namespace Examination_System.Controllers
         {
             return View();
         }
-
-        public IActionResult StudentExamAnswers(int ExamId,string StudentId)
-        {
-            //string rootpath = _hostingenvironment.contentrootpath;
-            //string reportpath = "d:\\iti\\projects\\examination-system\\examination system\\itiexamreports\\student_exam_answers.rdl";
-            //ReportViewer reportViewer = new ReportViewer();
-            //reportViewer.ProcessingMode = ProcessingMode.Local;
-            //reportviewer.sizetoreportcontent = true;
-            //reportViewer.LocalReport.ReportPath = reportpath;
-
-            //ViewBag.ReportViewer = reportViewer;
-            //int exId = 1;
-            //string stId = "12345678901234";
-            return Redirect($"http://localhost/ReportServer/Pages/ReportViewer.aspx?%2fITIExamReports%2fStudent_Exam_Answers&rs:Command=Render&examId={ExamId}&studentId={StudentId}");
-            //return View();
-        }
-        public IActionResult Info(string id)
-        {
-            var model = studentRepo.GetStudentById(id);
-            var track = model.Track;
-            var courses = studentRepo.GetCourses(id);
-            var branch = model.Branch.BranchName;
-            ViewBag.Track = track;
-            ViewBag.Branch = branch;
-            ViewBag.Courses = courses;
-            return View(model);
-        }
-        public IActionResult Courses(string id)
-        {
-            var model = studentRepo.GetStudentById(id);
-            var track = studentRepo.GetTrack(id);
-            var Courses = studentRepo.GetCourses(id);
-            ViewBag.track = track;
-            ViewBag.courses = Courses;
-            return View(model);
-        }
-        public IActionResult IncomingExams(string id)
-        {
-            var Exams = studentRepo.GetIncomingExamsForStudent(id);
-            var model = studentRepo.GetStudentById(id);
-            var track = studentRepo.GetTrack(id);
-            var branch = studentRepo.GetBranch(id);
-            return View(model);
-        }
-
         public IActionResult StudentPastExams()
         {
             List<StudentExamGrade> studentExamGrade = studentRepo.GetPastExams("13579246801357");
--- Examination System/Examination System/Repos/Student/StudentRepo.cs
+++ Examination System/Examination System/Repos/Student/StudentRepo.cs
@@ -14,39 +14,6 @@ namespace Examination_System.Repos.Student
             db = context;
             dbProcedures = _dbProcedures;
         }
-
-        public Models.Student GetStudentById(string studentId)
-        {
-            var student = db.Students.Include(s => s.Track).FirstOrDefault(s => s.StdId == studentId);
-            return student;
-        }
-
-        public Track GetTrack(string studentId)
-        {
-            var model = db.Students.Include(s => s.Track).ThenInclude(t=>t.Crs).FirstOrDefault(s=>s.StdId == studentId);
-            if (model == null)                
-                return null;
-
-            return model.Track;
-        }
-
-        public Branch GetBranch(string studentId)
-        {
-            var model = db.Students.Include(s=>s.Branch).FirstOrDefault(s => s.StdId == studentId);
-            if (model == null)
-                return null;
-
-            return model.Branch;
-        }
-
-        public ICollection<Course> GetCourses(string studentId)
-        {
-            var track = GetTrack(studentId);
-            if (track == null)
-                return [];
-
-            return track.Crs;
-        }
         public Models.Student GetStudentById(string studentId)
         {
             var student = db.Students.Include(s=> s.Track).Include(s=>s.Branch).Include(s=> s.User).FirstOrDefault(s => s.StdId == studentId);
